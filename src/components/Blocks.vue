a961cf24dbcd22477965b98cf17c1ede2da1d4a7
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-02-01 13:39:33 +0800

    add: lifecycle hooks mounted

---
+++ src/components/Blocks.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="blocks" v-if="showBlocks">
+        click me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlocks: false
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlocks = true
+        }, this.delay)
+    }
+}
+</script>
+
+<style scoped>
+    .blocks {
+        display: block;
+        width: 200px;
+        height: 300px;
+        background: green;
+        color: white;
+        margin: 1rem auto
+    }
+
+</style>
